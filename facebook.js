0e4418f5aa2ea0857b883fa6bda96fcf87219a15
nhattanphan0811 <64467078+nhattanphan0811@users.noreply.github.com>
2020-04-29 14:11:29 +0700

    Update facebook.js

--- facebook.js
+++ facebook.js
@@ -48,8 +48,8 @@ function Facebook(accessToken, groupId)
             `# TIN TỨC BUỔI TRƯA ${date}\n` +
             '\n' +
             '## thông tin\n' +
-            '* Bài viết được tự động đăng lúc 12 giờ trưa hàng ngày\n' +
-            '* Đây là post tự động được tạo bởi Lerni và lấy dữ liệu từ VnExpress qua RSS các bạn có thể check source code [tại đây](https://bit.ly/lerni-bot-fb)');
+            '* Bài viết được tự động đăng lúc 7 giờ sáng hàng ngày\n' +
+            '* Đây là post tự động được tạo bởi Tôi Và Kon Tum và lấy dữ liệu từ VnExpress qua RSS');
         formPublish.append('formatting', 'MARKDOWN');
 
         for (let i = 0; i < images.length ; i++) {
